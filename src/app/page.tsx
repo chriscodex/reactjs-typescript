792d2610776298f8954889e14b6218e328102320
Christian <c.espinoza.code@gmail.com>
2023-07-25 23:45:35 -0500

    images component added to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,49 @@
 'use client';
 import RandomFox from '@/components/RandomFox';
-import { ReactElement, useState } from 'react';
+import type { MouseEventHandler } from 'react';
+import { useState } from 'react';
 
 const randomNumber = (): number => {
   return Math.floor(Math.random() * 123) + 1;
 };
 
-export default function Home(): ReactElement {
-  const [images, setImages] = useState<string[]>([
-    `https://randomfox.ca/images/${randomNumber()}.jpg`,
-    `https://randomfox.ca/images/${randomNumber()}.jpg`,
-    `https://randomfox.ca/images/${randomNumber()}.jpg`,
-  ]);
+function generarIdUnico(): string {
+  const timestamp = new Date().getTime();
+  const random = Math.random().toString(36).substring(2);
+  return `${timestamp}-${random}`;
+}
+
+type image = {
+  id: string;
+  image: string;
+};
+
+export default function Home() {
+  const [images, setImages] = useState<image[]>([]);
+
+  const addImage: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault()
+    
+    setImages([
+      ...images,
+      {
+        id: generarIdUnico(),
+        image: `https://randomfox.ca/images/${randomNumber()}.jpg`,
+      },
+    ]);
+  };
 
   return (
-    <main>
-      <p>Hello</p>
-      {images.map((image, index) => {
-        return (
-          <div key={index}  className="mb-4">
+    <>
+      <div className="flex flex-col items-center">
+        <p className="text-center my-5">Home page</p>
+        <button onClick={(event) => addImage(event)}>Generar imagen</button>
+        {images.map(({ id, image }) => (
+          <div key={id} className="mb-5 ml-5">
             <RandomFox image={image} />
           </div>
-        );
-      })}
-    </main>
+        ))}
+      </div>
+    </>
   );
 }
